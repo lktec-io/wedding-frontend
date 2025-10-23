7478a3ce61eb6ceb1d49605f5473ed6a511c5b83
lktec-io <lktecio@gmail.com>
2025-10-23 21:55:34 +0300

    ------

--- src/Component/InvitationCard.jsx
+++ src/Component/InvitationCard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import QRCode from "react-qr-code";
 import inviteTemplate from "../assets/love.jpg";
-// import "./InvitationCard.css";
+
 
 export default function InvitationCard() {
   const { uuid } = useParams();
